e46a257640cec18f00ac212a86d806d81d50c0ce
Niladri <rickyrickado14@gmail.com>
2023-03-13 18:23:10 +0530

    added state management

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,23 @@
   <router-link to="/profile">Profile Page</router-link>
   <router-link to="/profile/nil">Profile Page</router-link>
   <router-view></router-view>
+  {{$store.state.name}}
+  {{$store.state.title}}
+  <button v-on:click="changeTitle()">Click</button>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  components: {
-    
+  methods:{
+    changeTitle(){
+      this.$store.commit({
+        type: 'changeTitle',
+        gender: 'male',
+        name: 'mickey'
+      })
+    }
   }
 }
 </script>
